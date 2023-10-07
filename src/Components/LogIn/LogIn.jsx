b80d8b32060bee4479df8746a2f1dea82f5dc939
“Rabeya <rabeyarupa7815@gmail.com>
2023-10-07 14:03:00 +0600

    Implemented active routes on NavBar

---
+++ src/Components/LogIn/LogIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogIn = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">This is LoginPage</h1>
+        </div>
+    );
+};
+
+export default LogIn;
---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">This is service page (hidden Route)</h1>
+        </div>
+    );
+};
+
+export default Services;
